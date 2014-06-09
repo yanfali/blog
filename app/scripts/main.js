0283ee81c60a443cab8aa19318114e3cf880440a
Yan-Fa li <yanfali@gmail.com>
2014-06-08 23:00:11 -0700

    Add a post click handler
    Make post card layouts clickable. Use a simple
    redirect using the permalink embedded as a data-permalink
    attribute.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,4 +8,13 @@ $(function() {
             $('.menu[data-id="' + section + '"]').addClass('active');
         }
     }
+
+    function postClickHandler(evt) {
+	evt.preventDefault();
+	var $post = $(evt.target).closest('.hugo-post');
+	var permalink = $post.attr('data-permalink');
+        window.location = permalink;
+    }
+
+    $('.hugo-post').on('click', postClickHandler);
 });
